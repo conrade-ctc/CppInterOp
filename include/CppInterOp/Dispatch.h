86d200243d3ae26031ccbc6103f52abf91779482
Emery Conrad <emery.conrad@chicagotrading.com>
2026-02-04 11:19:07 -0600

    allow reset of dispatch data to test load/unload

--- include/CppInterOp/Dispatch.h
+++ include/CppInterOp/Dispatch.h
@@ -25,6 +25,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <memory>
 #include <mutex>
 
 #ifdef _WIN32
@@ -191,14 +192,18 @@ extern "C" CPPINTEROP_API CppFnPtrTy CppGetProcAddress(const char* procname);
 // TODO: implement overload that takes an existing opened DL handle
 inline void* dlGetProcAddress(const char* name,
                               const char* customLibPath = nullptr) {
-  if (!name)
-    return nullptr;
+  static auto init = std::make_unique<std::once_flag>();
+  static void* (*getProc)(const char*);
 
-  static std::once_flag init;
-  static void* (*getProc)(const char*) = nullptr;
+  if (!name) {
+    // magic reset to keep static init inlined in function
+    init = std::make_unique<std::once_flag>();
+    getProc = nullptr;
+    return nullptr;
+  }
 
   // this is currently not tested in a multiple thread/process setup
-  std::call_once(init, [customLibPath]() {
+  std::call_once(*init, [customLibPath]() {
     const char* path =
         customLibPath ? customLibPath : std::getenv("CPPINTEROP_LIBRARY_PATH");
     if (!path)
@@ -211,6 +216,9 @@ inline void* dlGetProcAddress(const char* name,
           GetProcAddress(h, "CppGetProcAddress"));
       if (!getProc)
         FreeLibrary(h);
+    } else {
+      std::cerr << "[CppInterOp Dispatch] error code=" << GetLastError()
+                << "\n";
     }
 #else
     void* handle = dlopen(path, RTLD_LOCAL | RTLD_NOW);
@@ -219,6 +227,8 @@ inline void* dlGetProcAddress(const char* name,
       getProc = reinterpret_cast<void* (*)(const char*)>(
           dlsym(handle, "CppGetProcAddress"));
       if (!getProc) dlclose(handle);
+    } else {
+      std::cerr << "[CppInterOp Dispatch] " << dlerror() << "\n";
     }
 #endif
   });
@@ -286,6 +296,7 @@ inline void UnloadDispatchAPI() {
 #define DISPATCH_API(name, type) name = nullptr;
   CPPINTEROP_API_TABLE
 #undef DISPATCH_API
+  dlGetProcAddress(nullptr, nullptr); // this is a magic reset
 }
 } // namespace CppInternal::Dispatch
 
